4305e73e86849da57b36a6b4ac6068f14404ad2b
Feng Shu <tumashu@gmail.com>
2013-08-09 17:41:00 +0800

    Update eh-emms.el

--- eh-emms.el
+++ eh-emms.el
@@ -37,23 +37,45 @@
 (emms-default-players)
 (when (fboundp 'emms-cache) (emms-cache 1))
 
+;; EMMS 目录
 (setq emms-source-file-default-directory "~/Music")
+(setq emms-directory "~/Music/emms/")
+(setq emms-history-file "~/Music/emms/history")
+(setq emms-cache-file "~/Music/emms/cache")
+(setq emms-stream-bookmarks-file "~/Music/emms/streams")
+(setq emms-score-file "~/Music/emms/scores")
+
+;;设置 emms buffer name
 (setq emms-playlist-buffer-name "*Music*")
 
+;设定EMMS主模式为Playlist模式
 (setq emms-playlist-default-major-mode 'emms-playlist-mode)
+
+;修复播放完后的BUG
+(setq emms-player-next-function 'emms-next)
+
+;; 设定音轨初始化信息
 (add-to-list 'emms-track-initialize-functions 'emms-info-initialize-track)
 (setq emms-track-description-function 'eh-emms-info-track-description)
+;; 关闭EMMS信息异步模式
 (setq emms-info-asynchronously nil)
 
-;; Use faster finding facility if you have "GNU find"
+;; 设定EMMS启动列表循环播放
+(setq emms-repeat-playlist t)
+
+;; 排序方法: 艺术家 -> 专辑 -> 序号
+(setq emms-playlist-sort-function
+      'emms-playlist-sort-by-natural-order)
+
+;; 使用Gnu find查找文件
 (setq emms-source-file-directory-tree-function
       'emms-source-file-directory-tree-find)
 
-;; Show playing track in the minibuffer
+;; 在minibuffer中显示播放信息(emms-show)
 (add-hook 'emms-player-started-hook 'emms-show)
 (setq emms-show-format "正在播放: [%s]")
 
-;; Mode-line format
+;;设置Mode-line的显示方式
 (setq emms-mode-line-format "%s")
 (setq emms-playing-time-display-format "%s ]")
 (setq global-mode-string
@@ -74,10 +96,14 @@
                   (file-relative-name (emms-track-name track)
                                       emms-source-file-default-directory))))))
 
-;; lyrics
+;; 显示歌词
 (emms-lyrics 1)
 (setq emms-lyrics-display-on-modeline t)
 
+
+
+;; 设置Playlist的显示方式
+(setq emms-track-description-function 'eh-emms-info-track-description)
 (setq emms-last-played-format-alist
       '(((emms-last-played-seconds-today) . "%H:%M")
 	(604800                           . "%H:%M")
@@ -85,44 +111,100 @@
 	((emms-last-played-seconds-year)  . "%m-%d")
 	(t                                . "%Y")))
 
+(defun eh-emms-track-get-title (track)
+  (let ((name (emms-track-name track)))
+    (if (emms-track-get track 'info-title)
+        (emms-track-get track 'info-title)
+      (file-name-nondirectory name))))
+
+(defun eh-emms-track-get-info (track)
+  (let ((name-relative (file-relative-name
+                        (emms-track-name track)
+                        emms-source-file-default-directory)))
+    (if (emms-track-get track 'info-artist)
+        (emms-track-get track 'info-artist)
+      (if (file-name-directory name-relative)
+          (directory-file-name
+           (file-name-directory name-relative))
+        "未知"))))
+
+(defun eh-emms-track-get-artist (track)
+  (let ((info (eh-emms-track-get-info track)))
+    (if (file-name-directory info)
+        (directory-file-name (file-name-directory info))
+        info)))
+
+(defun eh-emms-track-get-album (track)
+  (let ((info (eh-emms-track-get-info track)))
+    (if (file-name-directory info)
+        (file-name-nondirectory info)
+        "")))
+
 (defun eh-emms-info-track-description (track)
   "Return a description of the current track."
   (let ((type (emms-track-type track))
         (play-count (or (emms-track-get track 'play-count) 0))
-        (last-played (or (emms-track-get track 'last-played) '(0 0 0))))
+        (last-played (or (emms-track-get track 'last-played) '(0 0 0)))
+        (name (emms-track-name track))
+        (pmin (emms-track-get track 'info-playing-time-min))
+        (psec (emms-track-get track 'info-playing-time-sec))
+        (ptot (emms-track-get track 'info-playing-time))
+        (title (eh-emms-track-get-title track))
+        (artist (eh-emms-track-get-artist track))
+        (album (eh-emms-track-get-album track)))
     (if (eq 'file type)
         (format "%5s %3s |-> %-s"
                 (emms-last-played-format-date last-played)
                 play-count
-                (if (and (emms-track-get track 'info-artist)
-                         (emms-track-get track 'info-title))
-                    (let ((pmin (emms-track-get track 'info-playing-time-min))
-                          (psec (emms-track-get track 'info-playing-time-sec))
-                          (ptot (emms-track-get track 'info-playing-time))
-                          (art  (emms-track-get track 'info-artist))
-                          (tit  (emms-track-get track 'info-title)))
-                      (cond ((and pmin psec) (format "%s -- %s [%02d:%02d]" art tit pmin psec))
-                            (ptot (format  "%s -- %s [%02d:%02d]" art tit (/ ptot 60) (% ptot 60)))
-                            (t (format "%s -- %s" art tit))))
-                  (replace-regexp-in-string "\\([^\\.]\\)/.*/" "\\1/.../"
-                   (file-relative-name (emms-track-name track)
-                                       emms-source-file-default-directory)))))))
+                (cond ((and pmin psec) (format "%s %s -- %s [%02d:%02d]" artist album title pmin psec))
+                      (ptot (format  "%s %s -- %s [%02d:%02d]" artist album title (/ ptot 60) (% ptot 60)))
+                      (t (format "%s %s -- %s" artist album  title)))))))
 
+;;; 设置EMMS 浏览器
+;; 默认显示方式为: 显示所有
+(emms-browser-set-filter (assoc "EVERYTHING" emms-browser-filters))
+;; filter: 显示所有
+(emms-browser-make-filter "EVERYTHING" 'ignore)
+;; filter: 只显示文件
+(emms-browser-make-filter "ALL-FILES" (emms-browser-filter-only-type 'file))
+;; filter: 最近一个星期播放的
+(emms-browser-make-filter "LAST-WEEK" (emms-browser-filter-only-recent 7))
+;; filter: 最近一个月都没有播放的
+(emms-browser-make-filter "LAST-MONTH-NOT-PLAYED" (lambda (track) (not (funcall (emms-browser-filter-only-recent 30) track))))
+;; EMMS 浏览器, 删除文件不提醒
+(put 'emms-browser-delete-files 'disabled nil)
+
+;; 设置 emms buffer 显示格式
+(setq emms-browser-info-artist-format "* %n")
+(setq emms-browser-info-album-format  "   `-> %n")
+(setq emms-browser-info-title-format  "        %n")
+(setq emms-browser-playlist-info-artist-format
+      emms-browser-info-artist-format)
+(setq emms-browser-playlist-info-album-format
+      emms-browser-info-album-format)
+(setq emms-browser-playlist-info-title-format
+      emms-browser-info-title-format)
+
+;; 如果歌曲没有tag,在Emms browser中使用目录名代替
+(setq emms-browser-get-track-field-function
+      'eh-emms-browser-get-track-field-simple)
+(defun eh-emms-browser-get-track-field-simple (track type)
+  (let ((title (eh-emms-track-get-title track))
+        (artist (eh-emms-track-get-artist track))
+        (album (eh-emms-track-get-album track)))
+    (if (emms-track-get track type)
+        (emms-track-get track type)
+      (case type
+        ('info-artist artist)
+        ('info-title  title)
+        ('info-album (if (eq album "") "misc" album))))))
+
+;; 快捷函数
 (defun eh-emms-toggle-playing ()
   (interactive)
   (if emms-player-playing-p
       (emms-pause)
     (emms-start)))
-
-(defun eh-emms-add-dired ()
-  (interactive)
-  (let ((buffer-dired (dired-noselect emms-source-file-default-directory)))
-    (if (eq major-mode 'dired-mode)
-      (progn
-        (emms-add-dired)
-        (emms-playlist-mode-go)
-        (kill-buffer buffer-dired))
-      (switch-to-buffer buffer-dired))))
 
 (defun eh-emms-search ()
   (interactive)
@@ -133,22 +215,34 @@
   (interactive)
   (if (or (null emms-playlist-buffer)
 	  (not (buffer-live-p emms-playlist-buffer)))
-      (eh-emms-add-dired))
+      (eh-emms-add-directory-tree))
   (emms-playlist-mode-go))
+
+(defun eh-emms-add-directory-tree ()
+  (interactive)
+  (emms-add-directory-tree
+   (ido-read-directory-name
+    "Add directory tree:"
+    emms-source-file-default-directory)))
+
+(defun eh-emms-add-file ()
+  (interactive)
+  (let ((file (ido-read-file-name
+               "Add directory tree:"
+               emms-source-file-default-directory)))
+    (cond
+     ((string-match "\\.\\(m3u\\|pls\\)\\'" file)
+      (emms-add-playlist file))
+     (t (emms-add-file file)))))
 
 ;; Global keybinding for emms
 (global-unset-key (kbd "C-c e"))
-(global-set-key (kbd "C-c e x") 'eh-emms)
-(global-set-key (kbd "C-c e d") 'eh-emms-add-dired)
-(global-set-key (kbd "C-c e f") 'emms-add-file)
-(global-set-key (kbd "C-c e l") 'emms-add-playlist)
-(global-set-key (kbd "C-c e a") 'emms-add-directory-tree)
+(global-set-key (kbd "C-c e e") 'eh-emms)
+(global-set-key (kbd "C-c e b") 'emms-smart-browse)
+(global-set-key (kbd "C-c e d") 'eh-emms-add-directory-tree)
+(global-set-key (kbd "C-c e f") 'eh-emms-add-file)
 
-(global-set-key (kbd "C-c e F") 'emms-play-file)
-(global-set-key (kbd "C-c e L") 'emms-play-playlist)
-(global-set-key (kbd "C-c e A") 'emms-play-directory-tree)
-
-(global-set-key (kbd "C-c e e") 'eh-emms-toggle-playing)
+(global-set-key (kbd "C-c e SPC") 'eh-emms-toggle-playing)
 (global-set-key (kbd "C-c e q") 'emms-stop)
 
 (global-set-key (kbd "C-c e n") 'emms-next)
@@ -164,6 +258,11 @@
 (global-set-key (kbd "C-c e s u") 'emms-score-up-playing)
 (global-set-key (kbd "C-c e s d") 'emms-score-down-playing)
 (global-set-key (kbd "C-c e s o") 'emms-score-show-playing)
+
+;; browser mode map
+(define-key emms-browser-mode-map (kbd "SPC") 'emms-browser-next-non-track)
+(define-key emms-browser-mode-map (kbd "C-SPC") 'emms-browser-next-non-track)
+(define-key emms-browser-mode-map (kbd "<tab>") 'emms-browser-toggle-subitems)
 
 ;; playlist-mode-map
 (define-key emms-playlist-mode-map (kbd "SPC") 'emms-pause)
