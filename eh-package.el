be1de8c6d8a90f55ec704f0e63ae52d54f82c8e9
Feng Shu <tumashu@gmail.com>
2014-03-16 19:31:19 +0800

    remove marmalade and tromey source

--- eh-package.el
+++ eh-package.el
@@ -1,10 +1,7 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("ELPA" . "http://tromey.com/elpa/") t)
+
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives 
              '("org" . "http://orgmode.org/elpa/") t)
 
