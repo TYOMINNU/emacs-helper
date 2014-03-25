55c4c6326d8c98b461d621d0d92ec57b25685f51
Feng Shu <tumashu@gmail.com>
2014-03-25 13:24:35 +0800

    update

--- emacs-packages-list.el
+++ emacs-packages-list.el
@@ -1,6 +1,7 @@
 ;; (require 'eh-package)
 ;; (insert (format "\n\n(let ((eh-packages '%s)) \n (eh-install-packages))" package-activated-list))
 
-(let ((eh-packages '(auctex audio-notes-mode auto-shell-command popwin deferred cdlatex ess-R-data-view ess popup ctable ess-R-object-popup ess popup ess-smart-underscore ess find-file-in-project flx-ido flx google-contacts oauth2 grep+ idle-highlight-mode ido-hacks ido-ubiquitous ido-vertical-mode magit git-rebase-mode git-commit-mode oauth2 org-gcal org gntp request-deferred request deferred package+ paredit plantuml-mode auto-complete popup popup-kill-ring pos-tip popup popwin pos-tip projectile pkg-info epl dash s recentf-ext request-deferred request deferred s smex yagist yasnippet))) 
+(let ((eh-packages '(ace-jump-buffer dash ace-jump-mode ace-jump-mode auctex audio-notes-mode auto-shell-command popwin deferred cdlatex ess-R-data-view ess popup ctable ess-R-object-popup ess popup ess-smart-underscore ess find-file-in-project flx-ido flx gntp google-contacts oauth2 grep+ idle-highlight-mode ido-hacks ido-ubiquitous ido-vertical-mode magit git-rebase-mode git-commit-mode mc-extras multiple-cursors multiple-cursors oauth2 package+ paredit phi-search plantuml-mode auto-complete popup popup-kill-ring pos-tip popup popwin pos-tip projectile pkg-info epl dash s recentf-ext request-deferred request deferred s smex yagist yasnippet))) 
  (eh-install-packages))
+
 
