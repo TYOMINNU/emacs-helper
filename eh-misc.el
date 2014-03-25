527f0b36190461f6cd6c06544a175a38bb624c1a
Feng Shu <tumashu@gmail.com>
2014-03-25 21:14:44 +0800

    update

--- eh-misc.el
+++ eh-misc.el
@@ -74,7 +74,7 @@
   "ace-jump-mode"
   "Emacs quick move minor mode"
   t)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-j") 'ace-jump-mode)
 
 ;; expand-region
 (require 'expand-region)
