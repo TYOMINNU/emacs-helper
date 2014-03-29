492ef986c626959b0a1233c681bb5e5431fcaa0d
Feng Shu <tumashu@gmail.com>
2014-03-29 10:38:53 +0800

    update eh-package.el

--- eh-package.el
+++ eh-package.el
@@ -2,17 +2,37 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives 
-             '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
+
+(defun eh-save-package-list ()
+  (interactive)
+  (let ((file (concat (file-name-directory
+		       (locate-library "eh-package.el"))
+		      "eh-packages-list.el"))
+	(pkg-list (delete-dups package-activated-list)))
+    (when (yes-or-no-p (concat "Do you want to save package list to file: " file "?"))
+      (save-excursion
+	(with-temp-file file
+	  (insert (concat ";; Update date: " (format-time-string "%Y-%m-%d") "\n\n"))
+	  (insert (format "(let ((eh-packages '%s)) (eh-install-packages-1))" pkg-list)))
+	(message "Save Success!")))))
+
+(defun eh-install-package ()
+  (interactive)
+  (let ((file (concat (file-name-directory
+		       (locate-library "eh-package.el"))
+		      "eh-packages-list.el"))
+	(pkg-list (delete-dups package-activated-list)))
+    (when (yes-or-no-p (concat "Do you want to install packages listed in file: " file "?"))
+      (load file))))
 
 (defun eh-packages-installed-p ()
   (loop for p in eh-packages
         when (not (package-installed-p p)) do (return nil)
         finally (return t)))
 
-(defun eh-install-packages ()
+(defun eh-install-packages-1 ()
   (unless (eh-packages-installed-p)
     ;; check for new packages (package versions)
     (message "%s" "Refreshing its package database...")
