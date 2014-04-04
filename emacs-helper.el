ce836e5a1ad73e02f0cd3bf0699b545971dff70d
Feng Shu <tumashu@gmail.com>
2014-04-05 07:55:34 +0800

    clean eh-misc.el and update emacs-helper.el

--- emacs-helper.el
+++ emacs-helper.el
@@ -1,6 +1,8 @@
 (load-library "eh-package")
 (load-library "eh-basic")
+(load-library "eh-mode-line")
 (load-library "eh-hanzi2pinyin")
+(load-library "eh-sdcv")
 (load-library "eh-emms")
 (load-library "eh-org")
 (load-library "eh-org-capture")
