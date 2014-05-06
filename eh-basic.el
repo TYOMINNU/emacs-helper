e3b07e4c63c3f5fa891be525b4edd3d75656d463
Feng Shu <tumashu@gmail.com>
2014-05-06 09:18:36 +0800

    update fonts settings

--- eh-basic.el
+++ eh-basic.el
@@ -52,7 +52,7 @@
 ;; 设置emacs-daemon使用tcp模式
 (setq server-use-tcp t)
 
-(defvar eh-default-fonts-list '("WenQuanYi Zen Hei Mono 15" "WenQuanYi Zen Hei Mono 15")
+(defvar eh-default-fonts-list '("PragmataPro 15")
   "Emacs的设计里，假设一个双字节字符和两个半角英文字符是等宽的。
 所有的表格对齐之类的问题，无论table-insert还是org-mode都是基于这个假设之上。
 可是实际上，这个假设不成立(字体问题)。我们一般找一对中英字体搭配使用，
@@ -65,34 +65,38 @@
    '(\"Liberation Mono 12\"  \"文泉驿等宽微米黑 15\")
    '(\"M+ 1m 12\" \"文泉驿等宽微米黑 12\")
 
+如果当前运行的emacs已经应用了CJK等宽补丁, 那么只需要
+选择一个字母宽度较小的英文等宽字体就可以了(字母宽度太大时，
+中文显示非常散)，比如：
+   '(\"PragmataPro 16\")
+
 注1: 文泉驿等宽正黑12号字体本身可以实现中英文对齐，
      不过显示效果没有文泉驿等宽微米黑好
 注2: WindowXP用户可以安装MacType软件，字体渲染效果较好！
 注3: Debian 6字体显示效果很差，可以安装infinality补丁，
-     显示效果有明显的提高。
-注4: 如果自己编译emacs，可以打CJK等宽补丁, 然后选择一个自己喜欢的等宽
-     英文字体就可以了，如果选择的等宽英文字母比较宽，中文就会显得
-     很散，看中文文献很难受，这时可以选择一个比较窄的英文等宽字体，
-     比如: PragmataPro")
-
+     显示效果有明显的提高。")
 
 ;; 设置字体的函数
 (defun eh-default-font ()
   (interactive)
-  (when window-system
-    (set-frame-font (car eh-default-fonts-list))
-    (dolist (charset '(kana han symbol cjk-misc bopomofo unicode)) 
-      (set-fontset-font (frame-parameter nil 'font) 
-                        charset (cdr eh-default-fonts-list)))))
+  (let ((default-font-name (car eh-default-fonts-list))
+	(cjk-font-name (car (cdr eh-default-fonts-list))))
+    (when window-system
+      (when default-font-name (set-frame-font default-font-name))
+      (when cjk-font-name
+	(dolist (charset '(kana han symbol cjk-misc bopomofo unicode))
+	  (set-fontset-font (frame-parameter nil 'font)
+			    charset cjk-font-name))))))
 
 ;; 设置默认字体
-;; (eh-default-font)
+(eh-default-font)
+
 ;; 使用daemon模式时的字体设置
-;; (if (and (fboundp 'daemonp) (daemonp))
-;;    (add-hook 'after-make-frame-functions
-;;	      (lambda (frame)
-;;		(with-selected-frame frame
-;;                  (eh-default-font)))))
+(if (and (fboundp 'daemonp) (daemonp))
+    (add-hook 'after-make-frame-functions
+	      (lambda (frame)
+		(with-selected-frame frame
+		  (eh-default-font)))))
 
 ;;;###autoload(require 'eh-basic)
 
