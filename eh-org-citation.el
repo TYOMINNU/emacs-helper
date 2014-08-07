522701437556b63826cc42144ba0d11f5132f249
Feng Shu <tumashu@gmail.com>
2014-08-07 23:51:46 +0800

    eh-jabref has been rewrite, my configure doesn't work

--- eh-org-citation.el
+++ eh-org-citation.el
@@ -39,6 +39,11 @@
 ;;; Code:
 
 (require 'ox-bibtex)
+(when (locate-library "org-odt")
+  (require 'ox-jabref))
+
+(setq eh-org-jabref-file "~/bin/JabRef-2.9.2.jar")
+(setq org-jabref-command (list "java" "-jar" (expand-file-name eh-org-jabref-file) "-n" "true"))
 
 ;; org cite link setting
 (org-add-link-type "cite" 'eh-ebib)
@@ -63,87 +68,6 @@
   (let ((orig-options (plist-get ad-return-value :options)))
     (setq options (plist-put ad-return-value :options
 			     (delete-dups (append eh-org-bibtex-bibtex2html-options orig-options))))))
-
-;; org-jabref cache directory
-(setq org-jabref-cache-directory "~/.org-jabref-cache")
-
-;; 官方org-mode没有包含ox-jabref.el，其包含在org-odt包中，
-(when (locate-library "org-odt")
-    (require 'ox-jabref))
-
-(setq eh-org-jabref-file "~/bin/JabRef-2.9.2.jar")
-(setq org-jabref-command (list "java" "-jar" (expand-file-name eh-org-jabref-file) "-n" "true"))
-
-(setq org-jabref-export-formats
-      '(("odt"
-	 ("GBT7714-2005"
-	  :in-text "GBT7714-2005.ODF.text" :bibliography "GBT7714-2005.ODF.reference"))))
-
-(setq org-jabref-odt-citation-transcoders
-      '(org-odt-citation-reference/numbered
-	. org-jabref-odt-bibliography/numbered))
-
-(defun org-jabref-group-consecutive-integers (integers)
-  "Partition a sorted list of integers into sorted lists of the consecutive integers that it contains.
-For example:
-    (org-jabref-group-consecutive-integers '(1 2 3 5 6 8 9 13 48))
-    => ((1 2 3) (5 6) (8 9) (13) (48))"
-  (let ((integres (delete-dups (sort integers '<)))
-	(previous-integer nil)
-	(this-set ())
-	(result ()))
-    (mapc (lambda (this-integer)
-	    (unless (integerp this-integer) (error "The set contains a non-integer."))
-	    (cond ((null this-set)
-		   (push this-integer this-set)
-		   (setq previous-integer this-integer))
-		  ((= this-integer (1+ previous-integer))
-		   (push this-integer this-set)
-		   (setq previous-integer this-integer))
-		  (t
-		   (setq this-set (nreverse this-set))
-		   (push this-set result)
-		   (setq this-set ())
-		   (push this-integer this-set)
-		   (setq previous-integer this-integer))))
-	  integers)
-    (cond ((not (null this-set))
-	   (setq this-set (nreverse this-set))
-	   (push this-set result)
-	   (nreverse result)))))
-
-(defun org-jabref-compress-citation-number (number-list)
-  (mapconcat
-   (lambda (list)
-     (let ((max (apply 'max list))
-	   (min (apply 'min list)))
-       (if (eq max min)
-	   (format "%s" max)
-	 (format "%s-%s" min max))))
-   (org-jabref-group-consecutive-integers number-list) ","))
-
-(defun org-odt-citation-reference/numbered (citation-reference contents info)
-  "Transcode a CITATION-REFERENCE element from Org to ODT.
-CONTENTS is nil.  INFO is a plist holding contextual information.
-
-Replace each CITE-KEY from CITATION-REFERENCE with it's numerical
-order in the exported Org file.  Return the concatenated result,
-after adding some separators."
-  (let* ((latex-frag (org-element-property :value citation-reference))
-	 (value (and (string-match org-odt-cite-regexp latex-frag)
-		     (substring (match-string 3 latex-frag) 1 -1)))
-	 (cite-keys (org-split-string value ",")))
-    (format "<text:span text:style-name=\"OrgSuperscript\">[%s]</text:span>"
-	    (org-jabref-compress-citation-number
-	     (mapcar
-	      (lambda (cite-key)
-		(setq cite-key (org-trim cite-key))
-		(let* ((citations-alist (plist-get info :citations-alist))
-		       (cite-key-entry (assoc cite-key citations-alist))
-		       (n (length (memq cite-key-entry citations-alist))))
-		  n))
-	      cite-keys)))))
-
 (provide 'eh-org-citation)
 ;; Local Variables:
 ;; coding: utf-8-unix
