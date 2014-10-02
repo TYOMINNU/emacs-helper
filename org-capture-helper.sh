df717cbaff3b85ebb4bf4762ce64ea025eadaea0
Feng Shu <tumashu@gmail.com>
2014-10-02 13:41:20 +0800

    update org-capture-helper.sh

--- org-capture-helper.sh
+++ org-capture-helper.sh
@@ -2,11 +2,13 @@
 
 set -efu
 
+EMACSCLIENT=/usr/local/tumashu-emacs/bin/emacsclient
+
 TEST=$(ps ax | grep "org-capture" | grep -v grep | wc -l)
 if [ $TEST = 1 ] ; then
-        exec emacsclient --server-file=default $*;
+        exec $EMACSCLIENT --socket-name=default $*;
 	exit 0;
 else
-        exec emacsclient --server-file=default -c -a ''  -F '((name . "org-capture"))' $*
+        exec $EMACSCLIENT --socket-name=default -c -a ''  -F '((name . "org-capture"))' $*;
         exit 0;
 fi
