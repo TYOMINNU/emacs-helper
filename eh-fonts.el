f9704a75594755450b6ad1e455376b013ee447c7
Feng Shu <tumashu@gmail.com>
2014-10-23 10:27:12 +0800

    update eh-fonts.el

--- eh-fonts.el
+++ eh-fonts.el
@@ -34,45 +34,83 @@
 (require 'cl-lib)
 
 (setq eh-font-size-conf "~/.emacs.d/emacs-font-size.conf")
-(setq eh-main-fonts
+(setq eh-fonts-alist
       '(("PragmataPro" "Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New")
-	("Microsoft Yahei" "Microsoft_Yahei" "微软雅黑" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体")))
-
-(setq eh-italic-fonts
-      '(("Courier New") ("Courier New")))
-
-(setq eh-bold-italic-fonts
-      '(("Courier New") ("Courier New")))
-
-(setq eh-symbol-fonts
-      '(("Courier New") ("Courier New")))
+	("文泉驿等宽微米黑" "Microsoft Yahei" "Microsoft_Yahei" "微软雅黑"  "黑体" "新宋体" "宋体")
+	("Courier New")
+	("Courier New")
+	("Courier New")
+	("Courier New")
+	("Courier New")
+	("Courier New")))
 
 (defvar eh-english-font-size nil)
 (defvar eh-english-font-size-steps '(9 10.5 11.5 12.5 14 16 18 20 22))
 
-(setq eh-chinese-font-size-scale-alist
-      (cond ((string-equal system-type "darwin")
-	     '((9    . 1.05) (10.5 . 1.05) (11.5 . 1.10)
-	       (12.5 . 1.10) (14   . 1.10) (16   . 1.05)
-	       (18   . 1.00) (20   . 1.05) (22   . 1.05)))
-	    ((string-equal system-type "windows-nt")
-	     '((9    . 1.05) (10.5 . 1.05) (11.5 . 1.10)
-	       (12.5 . 1.10) (14   . 1.10) (16   . 1.05)
-	       (18   . 1.00) (20   . 1.05) (22   . 1.05)))
-	    (t
-	     '((9    . 1.05) (10.5 . 1.05) (11.5 . 1.10)
-	       (12.5 . 1.10) (14   . 1.10) (16   . 1.05)
-	       (18   . 1.00) (20   . 1.05) (22   . 1.05)))))
+;; 调整系数中文字体的大小，从而实现中英文等宽。
+;; 调整时，可以使用`eh-test-font-scale-at-point'
+;; 来查看设置是否正确。
+(setq eh-fonts-size-scale-alist
+      '((1.05 1.05 1.10 1.10 1.10 1.05 1.00 0.98 0.85)
+	(1.05 1.05 1.10 1.10 1.10 1.05 1.00 0.98 0.85)
+	(1.05 1.05 1.10 1.10 1.10 1.05 1.00 0.98 0.85)))
 
-;; (eh-font-size-decrease)
-;; (eh-font-size-increase)
-;;
-;; 调整eh-chinese-font-size-scale-alist后，
-;; 运行上面两个命令，看下面的文字能否对齐.
-;; +----------------------------------------+
-;; | 一二三四五六七八九十九八七六五四三二一 |
-;; | abcdefghijklmnopqrstuvwxyz1234567890,. |
-;; +----------------------------------------+
+(defun eh-get-font-size-scale (&optional size)
+  (let* ((scale-list
+	  (cond ((string-equal system-type "darwin")
+		 (nth 0 eh-fonts-size-scale-alist))
+		((string-equal system-type "windows-nt")
+		 (nth 1 eh-fonts-size-scale-alist))
+		(t (nth 2 eh-fonts-size-scale-alist))))
+	 (index (position size eh-english-font-size-steps)))
+    (or (nth index scale-list) 1)))
+
+(defun eh-test-font-scale-at-point ()
+  "Test scale list at point, which is usd to write font scale list"
+  (interactive)
+  (let* ((buffer-name "*Show-font-effect*")
+	 (scale (sexp-at-point))
+	 (index
+	  (save-excursion
+	    (let* ((point1 (point))
+		   (point2 (progn (search-backward "(")
+				  (point))))
+	      (length (split-string
+		       (buffer-substring-no-properties point1 point2)
+		       " ")))))
+	 (size (nth (1- index) eh-english-font-size-steps)))
+    (eh-set-font size scale)
+    (eh-show-font-effect size scale)))
+
+(defun eh-show-font-effect (&optional size scale)
+  "show font and its size in a new buffer"
+  (interactive)
+  (with-output-to-temp-buffer buffer-name
+    (set-buffer buffer-name)
+    (when size
+      (insert (format "# 英文字体大小设置为: %s\n" size)))
+    (when scale
+      (insert (format "# 中文字体调整系数(scale)设置为: %s\n" scale)))
+    (insert
+     (replace-regexp-in-string
+      "\\^"  "\\\\"
+      (replace-regexp-in-string
+       "@@"  "   "
+       "
+# 请看下面中文和英文能否对齐.
+
++----------------------------------------+
+| 一二三四五六七八九十九八七六五四三二一 |
+| abcdefghijklmnopqrstuvwxyz,.1234567890 |
+| ABCDEFGHIJKLMNOPQRSTUVWXYZ,.!@#$%^&*() |
++----------------------------------------+
+
+  |^_/|            (^_/)
+ / @ @ \\  @@     (='.'=)
+( > º < )         (0)_(0)
+`>>x<<´
+/  O  \\  @@
+")))))
 
 (defun eh-font-exists-p (font)
   (if (null (x-list-fonts font))
@@ -98,8 +136,8 @@
 	(find-file eh-font-size-conf)
 	(goto-char (point-min))
 	(let ((font-size (read (current-buffer))))
-         (kill-buffer (current-buffer))
-         font-size))
+	  (kill-buffer (current-buffer))
+	  font-size))
     12.5))
 
 (defun eh-set-font (english-font-size &optional chinese-fonts-scale)
@@ -107,33 +145,24 @@
   (setq face-font-rescale-alist
 	(mapcar (lambda (x)
 		  (cons x chinese-fonts-scale))
-		(car (cdr eh-main-fonts))))
+		(nth 0 eh-fonts-alist)))
   (eh-set-font-internal english-font-size (or chinese-fonts-scale 1.2)))
 
 (defun eh-set-font-internal (english-font-size &optional chinese-fonts-scale)
   "english-font-size could be set to \":pixelsize=18\" or a integer.
 If set/leave chinese-font-size to nil, it will follow english-font-size"
-  (let ((english-main-font (eh-make-font-string
-			    (find-if #'eh-font-exists-p (car eh-main-fonts))
-			    english-font-size))
-	(chinese-main-font
-	 (font-spec :family
-		    (find-if #'eh-font-exists-p
-			     (car (cdr eh-main-fonts)))))
-	(english-italic-font
-	 (font-spec :family
-		    (find-if #'eh-font-exists-p
-			     (car eh-italic-fonts))))
-	(english-bold-italic-font
-	 (font-spec :slant 'italic :weight 'bold :size (+ 0.0 english-font-size)
-		    :family
-		    (find-if #'eh-font-exists-p
-			     (car eh-bold-italic-fonts))))
-	(english-symbol-font
-	 (font-spec :family
-		    (find-if #'eh-font-exists-p
-			     (car eh-bold-italic-fonts)))))
-
+  (let* ((english-main-font
+	  (eh-make-font-string (find-if #'eh-font-exists-p (nth 0 eh-fonts-alist))
+			       english-font-size))
+	 (chinese-main-font
+	  (font-spec :family (find-if #'eh-font-exists-p (nth 1 eh-fonts-alist))))
+	 (english-italic-font
+	  (font-spec :family (find-if #'eh-font-exists-p (nth 2 eh-fonts-alist))))
+	 (english-bold-italic-font
+	  (font-spec :slant 'italic :weight 'bold :size (+ 0.0 english-font-size)
+		     :family (find-if #'eh-font-exists-p (nth 4 eh-fonts-alist))))
+	 (english-symbol-font
+	  (font-spec :family (find-if #'eh-font-exists-p (nth 6 eh-fonts-alist)))))
     (set-face-attribute 'default nil :font english-main-font)
     (set-face-font 'italic english-italic-font)
     (set-face-font 'bold-italic english-bold-italic-font)
@@ -153,15 +182,14 @@ If set/leave chinese-font-size to nil, it will follow english-font-size"
     (setq next-size
           (cadr (member eh-english-font-size steps)))
     (when next-size
-      (eh-set-font next-size (cdr (assoc next-size eh-chinese-font-size-scale-alist)))
+      (eh-set-font next-size (eh-get-font-size-scale next-size))
       (eh-save-font-size-to-file next-size)
       (setq eh-english-font-size next-size)
       (message "Your font size is set to %.1f" next-size))))
 
 (defun eh-set-font-with-saved-size ()
   (let ((font-size (eh-read-font-size-from-file)))
-    (eh-set-font font-size
-		 (cdr (assoc font-size eh-chinese-font-size-scale-alist)))))
+    (eh-set-font font-size (eh-get-font-size-scale font-size))))
 
 ;; 正常启动emacs时设置字体
 (if (and (fboundp 'daemonp) (daemonp))
