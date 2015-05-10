3bc4da2a89c7f0c8e1ea4e74adc4065170aaa5fe
Feng Shu <tumashu@163.com>
2015-05-10 17:51:22 +0800

    update emacs daemon name

--- org-capture-helper.sh
+++ org-capture-helper.sh
@@ -4,9 +4,9 @@ set -efu
 
 TEST=$(ps ax | grep "org-capture" | grep -v grep | wc -l)
 if [ $TEST = 1 ] ; then
-	exec emacsclient --socket-name=default $*;
-	exit 0;
+    exec emacsclient --socket-name=sawfish-emacs-daemon $*;
+    exit 0;
 else
-	exec emacsclient --socket-name=default -c -a ''  -F '((name . "org-capture"))' $*;
-	exit 0;
+    exec emacsclient --socket-name=sawfish-emacs-daemon -c -a ''  -F '((name . "org-capture"))' $*;
+    exit 0;
 fi
